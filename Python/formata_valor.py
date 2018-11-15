a6efd26f64a7c27b8206e4bf5eeca4357bfb864a
Robson Totti <robsontotti15@gmail.com>
2018-11-15 15:51:03 -0200

    alterações python

--- Python/formata_valor.py
+++ Python/formata_valor.py
@@ -2,18 +2,26 @@
 
 
 def formata(msg):
-    msg = str(msg.split("[{u'Carimbo de data/hora': u'"))
+    #msg = str(msg.split("[{u'Carimbo de data/hora': u'"))
     #msg = str(msg.split("u'Temperatura': u"))
 
 
     data = ""
-    for i in range(6, 25):
+    for i in range(41, 60):
         data = data + msg[i]
 
     temperatura = ""
-    for i in range(46, 48):
+    for i in range(106, 111):
         temperatura = temperatura + msg[i]
 
-    estado = msg[60]
+    estado = msg[105]
+
+    print data
+    print temperatura
+    print estado
 
     return data, temperatura, estado
+
+
+    [{u'AY': u'', u'Carimbo de data/hora': u'08/11/2018 22:24:58', u'AZ': u'', u'Temperatura': u'', u'AX': u'025.80'}]
+
--- Python/setup.py
+++ Python/setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 import ibm_db
 from sheetsu import SheetsuClient
 
@@ -8,18 +9,46 @@ import formata_valor
 import adiciona_evento
 
 
-while True:
-    #try:
-    client = SheetsuClient("https://sheetsu.com/apis/v1.0su/4705062944af")
+# while True:
+try:
+    client = SheetsuClient("https://sheetsu.com/apis/v1.0su/7435ec86a4ee")
     mensagem = str(client.read(limit=1))
 
-    value = formata_valor.formata(mensagem)
+    msg = mensagem
+    aux = ""
+    for i in range(0, 2):
+        aux = aux + msg[i]
 
-    data = value[0]
-    temperatura = value[1]
-    estado = value[2]
+    print "AUX: " + aux
+    print mensagem
 
-    adiciona_evento.verifica_igualdade(data, temperatura, estado)
+    if(aux == "[{"):
 
-    #except:
-        #print("Erro ao buscar Informações - 502 Bad Gateway")
+        value = formata_valor.formata(mensagem)
+
+        data = value[0]
+        temperatura = value[1]
+        estado = value[2]
+
+        print value
+
+        adiciona_evento.verifica_igualdade(data, temperatura, estado)
+    else:
+        print "Erro ao buscar Informações - 502 Bad Gateway"
+
+except Exception, e:
+    #print("Erro ao buscar Informações - 502 Bad Gateway")
+    print e
+
+'''
+
+from sheetsu import SheetsuClient
+
+client = SheetsuClient("https://sheetsu.com/apis/v1.0su/7435ec86a4ee")
+# Read first two rows from sheet "Sheet2"
+#client.read(sheet="Sheet2", limit=2)
+
+mensagem = str(client.read(limit=1))
+
+print mensagem
+'''
